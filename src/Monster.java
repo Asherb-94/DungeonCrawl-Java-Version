573de5a0c9ef08e28c0ff946ea44883e87f0f558
Logan Barkes <lbarkes@ufl.edu>
2017-07-14 13:21:53 -0400

    Added: Class Hero Class Monster Class scene01 - part1 and part2
    Scene01 need functionality

---
+++ src/Monster.java
@@ -0,0 +1,77 @@
+
+public class Monster {
+	private int hitPoints;
+	  private int maxHitPoints;
+	  private int strength;
+	  private int maxStrength;
+	  private int potions;
+	  private int gold;
+	  private String name;
+	  
+	  public Monster(String name, int hitPoints, int strength, int gold){
+		  this.name = name;
+		  this.hitPoints = hitPoints;
+		  this.maxHitPoints = hitPoints;
+		  this.strength = strength;
+		  this.maxStrength = strength;
+		  this.gold = gold;
+	  }
+	  
+	  public void Summary(){
+		  System.out.println("Monster " + ": " + this.hitPoints + "/" 
+				  + this.maxHitPoints + "HP; " + this.strength + "/" + this.maxStrength 
+				  + " ST; " + this.gold + "Gold");
+	  }
+
+	public int getHitPoints() {
+		return hitPoints;
+	}
+
+	public void setHitPoints(int hitPoints) {
+		this.hitPoints = hitPoints;
+	}
+
+	public int getMaxHitPoints() {
+		return maxHitPoints;
+	}
+
+	public void setMaxHitPoints(int maxHitPoints) {
+		this.maxHitPoints = maxHitPoints;
+	}
+
+	public int getStrength() {
+		return strength;
+	}
+
+	public void setStrength(int strength) {
+		this.strength = strength;
+	}
+
+	public int getMaxStrength() {
+		return maxStrength;
+	}
+
+	public void setMaxStrength(int maxStrength) {
+		this.maxStrength = maxStrength;
+	}
+
+	public int getPotions() {
+		return potions;
+	}
+
+	public void setPotions(int potions) {
+		this.potions = potions;
+	}
+
+	public int getGold() {
+		return gold;
+	}
+
+	public void setGold(int gold) {
+		this.gold = gold;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+}
