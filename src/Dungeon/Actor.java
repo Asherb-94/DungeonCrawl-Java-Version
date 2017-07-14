ceb2052013160daa874f8c78d7cd64fa2b04ed34
Servo <robert.gatzak@my.ccsu.edu>
2017-07-14 13:47:09 -0400

    actor class

---
+++ src/Dungeon/Actor.java
@@ -0,0 +1,12 @@
+package Dungeon;
+
+public class Actor 
+{
+	public int hitPoints;
+	public int maxHitPoints;
+	public int strength;
+	public int maxStrength;
+	public int potions;
+	public int gold;
+	public char name[];
+}
